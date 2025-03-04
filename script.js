fb9fa4a00ac832165a8c276cceeb93a82931064e
Somivalich <andriysobko123321@email.com>
2025-03-04 15:35:15 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    alert("trigonometric and inverse trigonometric functions test.\ncreated by cznull@bilibili");
+
+    while (true) {
+        console.log("Зависання...");
+    }
+};
